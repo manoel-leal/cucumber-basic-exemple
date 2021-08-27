6735b85b681aeaf9d1696c3a1f763995c1b44957
mlpdleal <manoellealdeveloper@gmail.com>
2021-08-27 12:01:07 -0300

    Add runner class

--- bin/src/test/java/aprender_cucumber.feature
+++ bin/src/test/java/aprender_cucumber.feature
@@ -8,9 +8,4 @@ Funcionalidade: Aprender Cucumber
   Cenário: Deve executar a especificação
     Dado que criei o arquivo corretamente
     Quando executá-lo
-    Entao a especificação deve finalizar com sucesso
-    
-  Cenário: Deve incrementar contador
-  	Dado que o valor do contador é 15
-  	Quando eu incrementar em 3
-  	Entao o valor do contador deve ser 18
+    Então a especificação deve finalizar com sucesso
--- src/test/java/AprenderCucumber.java
+++ src/test/java/AprenderCucumber.java
@@ -1,25 +1,27 @@
 import cucumber.api.PendingException;
 import cucumber.api.java.pt.Dado;
-import cucumber.api.java.pt.Então;
+import cucumber.api.java.pt.Entao;
 import cucumber.api.java.pt.Quando;
 
 public class AprenderCucumber {
 	
 	@Dado("^que criei o arquivo corretamente$")
-	public void que_criei_o_arquivo_corretamente() {
-	    System.out.println("Passou aqui");
-	}
-
-	@Quando("^executá-lo$")
-	public void executá_lo() throws Throwable {
-		 // Write code here that turns the phrase above into concrete actions
-	    throw new PendingException();
-	}
-
-	@Então("^a especificação deve finalizar com sucesso$")
-	public void a_especificação_deve_finalizar_com_sucesso() throws Throwable {
+	public void queCrieiOArquivoCorretamente() throws Throwable {
 	    // Write code here that turns the phrase above into concrete actions
 	    throw new PendingException();
 	}
+
+	@Quando("^executá-lo$")
+	public void executáLo() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
+	@Entao("^a especificação deve finalizar com sucesso$")
+	public void aEspecificaçãoDeveFinalizarComSucesso() throws Throwable {
+	    // Write code here that turns the phrase above into concrete actions
+	    throw new PendingException();
+	}
+
 
 }
